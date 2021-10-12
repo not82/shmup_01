bdb09429222b94ed71f742f9be8dceb1dbd1cc5b
not82 <vendrouxf@gmail.com>
2021-10-12 14:17:09 +0200

    Added switch weapon/shield

--- shmup_01/Assets/Scripts/ShipController.cs
+++ shmup_01/Assets/Scripts/ShipController.cs
@@ -2,14 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using DefaultNamespace;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using Zenject;
 
 public class ShipController : IInitializable, ITickable
 {
+    public enum ActionMode
+    {
+        Weapon,
+        Shield
+    }
+
+    private ActionMode currentActionMode;
+
     public void Initialize()
     {
+        currentActionMode = ActionMode.Weapon;
         // Debug.Log(string.Join("\n", Gamepad.all));
     }
 
@@ -24,6 +34,11 @@ public class ShipController : IInitializable, ITickable
         if (Gamepad.current.aButton.wasPressedThisFrame)
         {
             fire();
+        }
+
+        if (Gamepad.current.bButton.wasPressedThisFrame)
+        {
+            switchWeaponShieldHandler();
         }
 
         if (Gamepad.current.leftStick.left.isPressed)
@@ -51,18 +66,39 @@ public class ShipController : IInitializable, ITickable
 
     private void fire()
     {
-        Debug.Log("FIRE!");
-        var bullet = bulletFactory.Spawn();
-        bullet.Position = new Vector3(bulletOriginTransform.position.x, bulletOriginTransform.position.y);
-        bullet.Velocity = new Vector3(0f, 10f);
+        // Debug.Log("FIRE!");
+
+        if (currentActionMode == ActionMode.Weapon)
+        {
+            var bullet = bulletFactory.Spawn();
+            bullet.Position = new Vector3(bulletOriginTransform.position.x, bulletOriginTransform.position.y);
+            bullet.Velocity = new Vector3(0f, 10f);
+        }
+    }
+
+    private void switchWeaponShieldHandler()
+    {
+        Debug.Log("SWITCH!");
+        switch (currentActionMode)
+        {
+            case ActionMode.Weapon:
+                rotationPoint.DOLocalRotate(new Vector3(0, 0, 180), 0.5f);
+                currentActionMode = ActionMode.Shield;
+                break;
+            case ActionMode.Shield:
+                rotationPoint.DOLocalRotate(new Vector3(0, 0, 0), 0.5f);
+                currentActionMode = ActionMode.Weapon;
+                break;
+        }
     }
 
     [Inject(Id = "Ship")] private Transform transform;
 
-    [Inject(Id = "Ship/Weapon")]
-    private Transform bulletOriginTransform;
+    [Inject(Id = "Ship/Weapon")] private Transform bulletOriginTransform;
 
     [Inject] private Bullet.Pool bulletFactory;
     [Inject] private Bullet.Pool2 bulletFactory2;
     [Inject] private ShmupSettings _shmupSettings;
+
+    [Inject(Id = "Ship/RotationPoint")] private Transform rotationPoint;
 }
